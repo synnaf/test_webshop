fb406eeffd6adaf6ed16c27faa73000c05e47147
[synnaf] <[f.vforsman@gmail.com]>
2020-02-26 12:23:04 +0100

    testing new commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const dbURL = process.env.MONGO_ATLAS_URL || require("./config/config").databaseURL
 //Här startar vi vår server 
-const {app, port} = require("./src")
+const {app, port} = require("./src/server")
 
 //Kicka igång servern 
 const dbOptions = {useUnifiedTopology: true, useNewUrlParser: true}
